d94becfeeb0185930e445ff28dc7c62189e97af2
Dennis <d.loska@hotmail.de>
2018-12-25 05:05:24 +0100

    adding faker.js props to CommentDetail via parent component

--- components-02/src/CommentDetail.js
+++ components-02/src/CommentDetail.js
@@ -1,27 +1,22 @@
-import React from 'react';
-import faker from 'faker';
+import React from "react";
 
 const CommentDetail = props => {
-    return (
-        <div className="comment">
-            <a className="avatar" href="/">
-                <img alt="avatar" src={faker.image.avatar()}/>
-            </a>
-            <div className="content">
-                <a className="author" href="/">
-                    {props.author}
-                </a>
-                <div className="metadata">
-                    <span className="date">
-                        {props.date}
-                    </span>
-                </div>
-                <div className="text">
-                    {props.text}
-                </div>
-            </div>
+  return (
+    <div className="comment">
+      <a className="avatar" href="/">
+        <img alt="avatar" src={props.avatar} />
+      </a>
+      <div className="content">
+        <a className="author" href="/">
+          {props.author}
+        </a>
+        <div className="metadata">
+          <span className="date">{props.date}</span>
         </div>
-    );
-}
+        <div className="text">{props.text}</div>
+      </div>
+    </div>
+  );
+};
 
 export default CommentDetail;
--- components-02/src/index.js
+++ components-02/src/index.js
@@ -1,22 +1,34 @@
 // 1. importing React
-import React from 'react';
-import ReactDOM from 'react-dom';
-import faker from 'faker';
-import CommentDetail from './CommentDetail';
+import React from "react";
+import ReactDOM from "react-dom";
+import faker from "faker";
+import CommentDetail from "./CommentDetail";
 
 // 2. creating the components
 const App = () => {
-    return (
-        <div className="ui container comments">
-            <CommentDetail author={faker.name.firstName()} date={faker.date.recent().toDateString()} text={faker.lorem.sentence()} />
-            <CommentDetail author={faker.name.firstName()} date={faker.date.recent().toDateString()} text={faker.lorem.sentence()} />
-            <CommentDetail author={faker.name.firstName()} date={faker.date.recent().toDateString()} text={faker.lorem.sentence()} />
-        </div>
-    );
+  return (
+    <div className="ui container comments">
+      <CommentDetail
+        avatar={faker.image.avatar()}
+        author={faker.name.firstName()}
+        date={faker.date.recent().toDateString()}
+        text={faker.lorem.sentence()}
+      />
+      <CommentDetail
+        avatar={faker.image.avatar()}
+        author={faker.name.firstName()}
+        date={faker.date.recent().toDateString()}
+        text={faker.lorem.sentence()}
+      />
+      <CommentDetail
+        avatar={faker.image.avatar()}
+        author={faker.name.firstName()}
+        date={faker.date.recent().toDateString()}
+        text={faker.lorem.sentence()}
+      />
+    </div>
+  );
 };
 
 // 3. render the components
-ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
-);
+ReactDOM.render(<App />, document.querySelector("#root"));
