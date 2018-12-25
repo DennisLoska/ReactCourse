16445bbd9d19c88fee23d97316cb6f15028557b5
Dennis <d.loska@hotmail.de>
2018-12-25 21:36:42 +0100

    working with class components and conditional rendering using state

--- seasons-03/src/index.js
+++ seasons-03/src/index.js
@@ -9,9 +9,34 @@ const App = () => {
     position => console.log(position),
     err => console.log(err)
   );
-  return <div>Hi there!</div>;
+  return <div>Latitude: </div>;
 };
 
 */
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { lat: null, errorMessage: '' };
+    this.getLocation();
+  }
+
+  getLocation() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
+    );
+  }
+
+  render() {
+    if (!this.state.lat && this.state.errorMessage) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+    if (this.state.lat && !this.state.errorMessage) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+    return <div>Loading...</div>;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector('#root'));
