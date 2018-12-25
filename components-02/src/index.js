71390a33c6b2c5591d991cae9cc3251c483fee90
Dennis <d.loska@hotmail.de>
2018-12-25 05:55:16 +0100

    adding ApprovalCard and reusing CommentDetail components in ApprovalCard

--- components-02/src/index.js
+++ components-02/src/index.js
@@ -3,29 +3,36 @@ import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 // 2. creating the components
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        avatar={faker.image.avatar()}
-        author={faker.name.firstName()}
-        date={faker.date.recent().toDateString()}
-        text={faker.lorem.sentence()}
-      />
-      <CommentDetail
-        avatar={faker.image.avatar()}
-        author={faker.name.firstName()}
-        date={faker.date.recent().toDateString()}
-        text={faker.lorem.sentence()}
-      />
-      <CommentDetail
-        avatar={faker.image.avatar()}
-        author={faker.name.firstName()}
-        date={faker.date.recent().toDateString()}
-        text={faker.lorem.sentence()}
-      />
+      <ApprovalCard>
+        <CommentDetail
+          avatar={faker.image.avatar()}
+          author={faker.name.firstName()}
+          date={faker.date.recent().toDateString()}
+          text={faker.lorem.sentence()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          avatar={faker.image.avatar()}
+          author={faker.name.firstName()}
+          date={faker.date.recent().toDateString()}
+          text={faker.lorem.sentence()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          avatar={faker.image.avatar()}
+          author={faker.name.firstName()}
+          date={faker.date.recent().toDateString()}
+          text={faker.lorem.sentence()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
