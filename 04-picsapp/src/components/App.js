f4e128e19723a769edbb50737e61aea3b925438a
Dennis <d.loska@hotmail.de>
2018-12-26 05:27:10 +0100

    creating get-request using axios

--- 04-picsapp/src/components/App.js
+++ 04-picsapp/src/components/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
+import Config from '../config';
 
 class App extends React.Component {
   onFormSubmit(term) {
-    console.log(term);
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization: 'Client-ID ' + new Config().getAccess()
+      }
+    });
   }
   render() {
     return (
@@ -15,3 +22,19 @@ class App extends React.Component {
 }
 
 export default App;
+
+/* in order to restore Config.js:
+
+class Config {
+  constructor() {
+    this.access =
+      'your access-key';
+  }
+  getAccess() {
+    return this.access;
+  }
+}
+
+export default Config;
+
+*/
