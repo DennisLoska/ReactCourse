80e36118665a56d54402107104c0eed8c070bbda
Dennis <d.loska@hotmail.de>
2019-01-08 11:04:18 +0100

    restructuring project

---
+++ 06-songs/src/components/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import SongDetail from './SongDetail';
+import SongList from './SongList';
+import './App.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { selectedSong: {} };
+    this.songs = [
+      {
+        title: 'Psalm 51',
+        length: '4:36'
+      },
+      {
+        title: 'At the cross',
+        length: '3:27'
+      },
+      {
+        title: 'Narrow road',
+        length: '2:56'
+      }
+    ];
+  }
+
+  selectSong = song => {
+    this.setState({ selectedSong: song });
+  };
+
+  render() {
+    return (
+      <div style={{ display: 'flex' }} className="songs-container">
+        <div className="song-list">
+          <SongList songs={this.songs} selectSong={this.selectSong} />
+        </div>
+        <SongDetail song={this.state.selectedSong} />
+      </div>
+    );
+  }
+}
+
+export default App;
--- 06-songs/src/index.js
+++ 06-songs/src/index.js
@@ -1,43 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SongDetail from './SongDetail';
-import SongList from './SongList';
-import './index.css';
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { selectedSong: {} };
-    this.songs = [
-      {
-        title: 'Psalm 51',
-        length: '4:36'
-      },
-      {
-        title: 'At the cross',
-        length: '3:27'
-      },
-      {
-        title: 'Narrow road',
-        length: '2:56'
-      }
-    ];
-  }
-
-  selectSong = song => {
-    this.setState({ selectedSong: song });
-  };
-
-  render() {
-    return (
-      <div style={{ display: 'flex' }} className="songs-container">
-        <div className="song-list">
-          <SongList songs={this.songs} selectSong={this.selectSong} />
-        </div>
-        <SongDetail song={this.state.selectedSong} />
-      </div>
-    );
-  }
-}
+import App from './components/App';
 
 ReactDOM.render(<App />, document.querySelector('#root'));
