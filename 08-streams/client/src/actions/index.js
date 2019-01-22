f5da2289933ea035fe0094e97178119f9b007130
Dennis <d.loska@hotmail.de>
2019-01-22 17:45:10 +0100

    adding CRUD action creators

--- 08-streams/client/src/actions/index.js
+++ 08-streams/client/src/actions/index.js
@@ -1,4 +1,12 @@
-import { SIGN_IN, SIGN_OUT, FETCH_STREAMS, CREATE_STREAM } from './types';
+import {
+  SIGN_IN,
+  SIGN_OUT,
+  FETCH_STREAMS,
+  CREATE_STREAM,
+  FETCH_STREAM,
+  UPDATE_STREAM,
+  DELETE_STREAM
+} from './types';
 import streams from '../apis/streams';
 
 export const signIn = id => {
@@ -16,11 +24,23 @@ export const signOut = id => {
 };
 
 export const createStream = formValues => async dispatch => {
-  streams.post('/streams', formValues);
-  dispatch({
-    type: CREATE_STREAM,
-    payload: formValues
-  });
+  const response = await streams.post('/streams', formValues);
+  dispatch({ type: CREATE_STREAM, payload: response.data });
+};
+
+export const updateStream = (id, formValues) => async dispatch => {
+  const response = await streams.put(`streams/${id}`, formValues);
+  dispatch({ type: UPDATE_STREAM, payload: response.data });
+};
+
+export const deleteStream = id => async dispatch => {
+  await streams.delete(`streams/${id}`);
+  dispatch({ type: DELETE_STREAM, payload: null });
+};
+
+export const fetchStream = id => async dispatch => {
+  const response = await streams.get(`streams/${id}`);
+  dispatch({ type: FETCH_STREAM, payload: response.data });
 };
 
 export const fetchStreamList = () => async dispatch => {
--- 08-streams/client/src/reducers/streamReducer.js
+++ 08-streams/client/src/reducers/streamReducer.js
@@ -1,8 +1,23 @@
-import { FETCH_STREAMS } from '../actions/types';
-export default (state = [], action) => {
-  switch (action.type) {
+import {
+  FETCH_STREAMS,
+  CREATE_STREAM,
+  FETCH_STREAM,
+  DELETE_STREAM,
+  UPDATE_STREAM
+} from '../actions/types';
+
+export default (state = [], { type, payload }) => {
+  switch (type) {
+    case FETCH_STREAM:
+      return payload;
     case FETCH_STREAMS:
-      return action.payload;
+      return payload;
+    case CREATE_STREAM:
+      return payload;
+    case UPDATE_STREAM:
+      return payload;
+    case DELETE_STREAM:
+      return payload;
     default:
       return state;
   }
