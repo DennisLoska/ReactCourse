4b427172f17bf4d0d38a94f6f3ed7ef68c34bfed
Dennis <d.loska@hotmail.de>
2019-01-23 18:51:14 +0100

    fixing typo in types.js and loading API request in separate components

--- 08-streams/client/src/components/streams/StreamEdit.js
+++ 08-streams/client/src/components/streams/StreamEdit.js
@@ -1,9 +1,50 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
 
 class StreamEdit extends React.Component {
+  // DO NOT DO THIS because of ownProps in mapStateToProps function direct access to index
+  // getStreamDetails = () => {
+  //   return this.props.streams.map(stream => {
+  //     if (stream.id.toString() === this.props.match.params.id) {
+  //       return (
+  //         <div key={stream.id}>
+  //           <div>{stream.title}</div>
+  //           <div>{stream.description}</div>
+  //         </div>
+  //       );
+  //     } else return null;
+  //   });
+  // };
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  renderEdit() {
+    console.log(this.props);
+    if (this.props.stream) {
+      return (
+        <div>
+          StreamEdit
+          <div> {this.props.stream.title}</div>
+          <div> {this.props.stream.description}</div>
+        </div>
+      );
+    } else return <div>Loading...</div>;
+  }
+
   render() {
-    return <div>StreamEdit</div>;
+    return this.renderEdit();
   }
 }
 
-export default StreamEdit;
+const mapStateToProps = (state, ownProps) => {
+  return {
+    stream: state.streams[ownProps.match.params.id]
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { fetchStream }
+)(StreamEdit);
