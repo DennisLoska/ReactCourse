cc9013c3354db94f550e1059a87dae9355ba5a6a
Dennis <d.loska@hotmail.de>
2019-02-18 15:59:24 +0100

    refactoring onDismiss into parent component of Modal as a prop

--- 08-streams/client/src/components/streams/StreamDelete.js
+++ 08-streams/client/src/components/streams/StreamDelete.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { fetchStream } from '../../actions';
 import Modal from '../Modal';
+import history from '../history';
 
 class StreamDelete extends React.Component {
   actions = (
@@ -24,6 +25,7 @@ class StreamDelete extends React.Component {
             title="Delete Stream"
             content="Are you sure you want to delete this stream?"
             actions={this.actions}
+            onDismiss={() => history.push('/')}
           />
         </div>
       );
