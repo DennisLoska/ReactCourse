b6558ae25659f406cbfae95bbe47cb9fb19e5362
Dennis <d.loska@hotmail.de>
2019-02-19 13:28:54 +0100

    adding flvPlayer with flv.js to show stream from RTMP server

--- 08-streams/client/src/components/streams/StreamShow.js
+++ 08-streams/client/src/components/streams/StreamShow.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetchStream } from '../../actions';
+import flv from 'flv.js';
 
 class StreamShow extends React.Component {
+  constructor(props) {
+    super(props);
+    this.videoRef = React.createRef();
+  }
   componentDidMount() {
-    this.props.fetchStream(this.props.match.params.id);
+    const { id } = this.props.match.params;
+    this.props.fetchStream(id);
+    this.buildPlayer();
+  }
+
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  buildPlayer() {
+    if (this.flvPlayer || !this.props.stream) return;
+    const { id } = this.props.match.params;
+    this.flvPlayer = flv.createPlayer({
+      type: 'flv',
+      url: `http://localhost:8000/live/${id}.flv`
+    });
+    this.flvPlayer.attachMediaElement(this.videoRef.current);
+    this.flvPlayer.load();
   }
 
   render() {
@@ -12,9 +34,9 @@ class StreamShow extends React.Component {
       const { title, description } = this.props.stream;
       return (
         <div>
-          StreamShow
-          <div> {title}</div>
-          <div> {description}</div>
+          <video ref={this.videoRef} style={{ width: '100%' }} controls />
+          <h1> {title}</h1>
+          <p> {description}</p>
         </div>
       );
     } else return <div>Loading...</div>;
