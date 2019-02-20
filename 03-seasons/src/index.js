10a5214a43826235beeac8d5abbb83d9d6cd31ee
Dennis <d.loska@hotmail.de>
2019-02-20 16:58:26 +0100

    making hook reusable

--- 03-seasons/src/index.js
+++ 03-seasons/src/index.js
@@ -2,23 +2,14 @@ import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 import Spinner from './Spinner';
+import useLocation from './useLocation';
 
 const App = () => {
-  const [lat, setLat] = useState(null);
-  const [error, setError] = useState('');
-
-  useEffect(() => {
-    window.navigator.geolocation.getCurrentPosition(
-      position => setLat(position.coords.latitude),
-      err => setError(err.message)
-    );
-  }, []);
-
+  const [lat, error] = useLocation();
   let content;
   if (!lat && error) content = <div>Error: {error}</div>;
   else if (lat && !error) content = <SeasonDisplay lat={lat} />;
   else content = <Spinner message="Please accept the location request" />;
-
   return <div className="demo-wrapper">{content}</div>;
 };
 
